9cb6a81187ff020a812705c849ef35c66036d569
Lee Jiangwei <ljwskrskr@outlook.com>
2019-09-07 22:05:36 +0800

    Refactor csvfilereader with generic

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,31 +1,20 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
+  abstract mapRow(row: string[]): T
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
-        encoding: 'utf-8'
+        encoding:  'utf-8'
       })
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, // type assertion in Typescript
-          row[6]
-        ];
-      });
+      .map(this.mapRow);
   }
 }
