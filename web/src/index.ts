5548506d22bfe3663cc3650b4e8fd656981d480c
Lee Jiangwei <ljwskrskr@outlook.com>
2019-09-21 20:40:01 +0800

    Finish up to unit 174

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,13 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'smz', age: 50 });
+const user = new User({ id: 1, name: 'smzsb' });
 
-console.log(user.get('name'));
 user.on('change', () => {
-  console.log('changed');
+  console.log(user);
 });
-user.trigger('change');
-     
+
+user.on('save', () => {
+  console.log('saved');
+});
+
+user.save();
