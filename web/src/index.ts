2ec3ddd018809852e196959c2e231bb6fe97b760
Lee Jiangwei <ljwskrskr@outlook.com>
2019-09-21 19:17:32 +0800

    Finish up to unit 171

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,10 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'smz' });
+const user = new User({ name: 'smz', age: 50 });
 
-user.events.on('change', () => {
-  console.log('changed!');
+console.log(user.get('name'));
+user.on('change', () => {
+  console.log('changed');
 });
-user.events.trigger('change');
+user.trigger('change');
+     
---
+++ web/src/models/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // "values of K" can only be one of "keys of T"
+  get = <K extends keyof T>(key: K): T[K] => {
+    return this.data[key];
+  };
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
