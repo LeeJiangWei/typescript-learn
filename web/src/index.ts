04c612b6050bae58a4aa9ec1eae83cfd61c61449
Lee Jiangwei <ljwskrskr@outlook.com>
2019-09-22 11:48:18 +0800

    Finish up to unit 185

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,19 @@
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
+import axios from 'axios';
+import { Collection } from './models/Collection';
 
-const user = User.buildUser({ id: 1 });
+// const user = User.buildUser({ id: 1 });
 
-user.on('change', () => {
-  console.log(user);
+// user.on('change', () => {
+//   console.log(user);
+// });
+
+// user.fetch();
+
+const collection = User.buildUserCollection();
+
+collection.on('change', () => {
+  console.log(collection);
 });
 
-user.fetch();
+collection.fetch();
